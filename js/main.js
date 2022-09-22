0884bd1d3fd0639e679148198bd4cd780505eecb
nikkolet <nikkoletashby@Gmail.com>
2022-09-22 12:06:59 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
 const links = {
-    w1_label: "Week1 Notes",
-    w1_url: "week1/index.html",
-    w2_label: "Week2 Notes",
-    w2_url: "week2/index.html",
-    w3_label: "week3 Notes",
-    w3_url: "week3/index.html"
+    w1Label: "Week1 Notes",
+    w1Url: "week1/index.html",
+    w2Label: "Week2 Notes",
+    w2Url: "week2/index.html",
+    w3Label: "week3 Notes",
+    w3Url: "week3/index.html",
+    w4Label: "Week4 Notes",
+    w4Url: "week4/index.html",
+    w5Label: "Week5 Notes",
+    w5Url: "week5/index.html",
+    w6Label: "week6 Notes",
+    w6Url: "week6/index.html",
+    w7Label: "Week7 Notes",
+    w7Url: "week7/index.html",
+    w8Label: "Week8 Notes",
+    w8Url: "week8/index.html",
+    w9Label: "week9 Notes",
+    w9Url: "week9/index.html",
+    w10Label: "Week10 Notes",
+    w10Url: "week10/index.html",
+    w11Label: "Week11 Notes",
+    w11Url: "week11/index.html",
+    w12Label: "week12 Notes",
+    w12Url: "week12/index.html",
+    w13Label: "Week13 Notes",
+    w13Url: "week13/index.html",
+    w14Label: "Week14 Notes",
+    w1Url: "week14/index.html"
 };
 
 
-document.getElementById("list").innerHTML = `<li><a href=${links.url1}>${links.label1}</a></li>
-<li><a href=${links.url2}>${links.label2}</a></li>`;
+document.getElementById("list").innerHTML = `
+<li><a href=${links.w1Label}>${links.w1Url}</a></li>
+<li><a href=${links.w2Label}>${links.w2Url}</a></li>
+<li><a href=${links.w3Label}>${links.w3Url}</a></li>
+<li><a href=${links.w4Label}>${links.w4Url}</a></li>
+<li><a href=${links.w5Label}>${links.w5Url}</a></li>
+<li><a href=${links.w6Label}>${links.w6Url}</a></li>
+<li><a href=${links.w7Label}>${links.w7Url}</a></li>
+<li><a href=${links.w8Label}>${links.w8Url}</a></li>
+<li><a href=${links.w9Label}>${links.w9Url}</a></li>
+<li><a href=${links.w10Label}>${links.w10Url}</a></li>
+<li><a href=${links.w11Label}>${links.w11Url}</a></li>
+<li><a href=${links.w12Label}>${links.w12Url}</a></li>
+<li><a href=${links.w13Label}>${links.w13Url}</a></li>
+<li><a href=${links.w14Label}>${links.w14Url}</a></li>`;
