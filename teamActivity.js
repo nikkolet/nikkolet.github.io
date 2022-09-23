75155443658edf525ffc4d1361094526c3ddd584
nikkolet <nikkoletashby@Gmail.com>
2022-09-23 08:32:23 -0600

    commit

---
+++ teamActivity.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick = function() {
+    let myNum = document.getElementById("myText").value;
+
+
+    document.getElementById("output").innerHTML = "Output: " + myNum;
+
+}
