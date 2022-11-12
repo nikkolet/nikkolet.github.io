cf636e3b33ca01003c8278c5a9441ad5a05d9050
nikkolet <nikkoletashby@Gmail.com>
2022-11-11 19:38:51 -0700

    commit

---
+++ week9/js/main.js
@@ -0,0 +1,15 @@
+let keys = document.querySelector('keys');
+
+// msg.addEventListener("keydown", (event) => {
+//     // handle keydown
+// });
+
+// msg.addEventListener("keypress", (event) => {
+//     // handle keypress
+// });
+
+// msg.addEventListener("keyup", (event) => {
+//     // handle keyup
+// });
+
+keys.forEach(element => console.log(element));
