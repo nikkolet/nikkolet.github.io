2f6fb98b11d1b12e75cc44496230c1dacc43b52d
nikkolet <nikkoletashby@Gmail.com>
2024-10-07 10:17:23 -0600

    Update
    Projects completed update

---
+++ c#_projects/Calculator/Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            int result = 0; //You need to assign it 0 as a value to avoid error cs0165. I did this so that if the user accidently mistypes the operation it can let the user know. I am still working out how to reset the program as needed.
+
+            string answer;
+
+            Console.WriteLine("Hello, welcome to the calculator program!");
+            Console.WriteLine("Please enter your first number: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine()); //ToInt32 converts string to int
+
+            Console.WriteLine("Please enter your second number.");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("What type of operation would you liked to do?");
+            Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication or d for division.");
+
+            answer = Console.ReadLine();
+
+            if(answer == "a" || answer == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (answer == "s" || answer == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (answer == "m" || answer == "*")
+            {
+                result = num1 * num2;
+            }
+            else if (answer == "d" || answer == "/")
+            {
+                result = num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine($"Oops! Try again. You gave {answer}");
+                
+            }
+            
+            Console.WriteLine($"The result is: {result}");
+            Console.WriteLine("Thank you for using the calculator program");
+
+            Console.ReadKey(); //Program ends when you enter a key. You could also put "Console.Read();"
+
+        }
+    }
+}
---
+++ projects/Dice_Game/Dice_Game/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+            int round = 0;
+            int maxRounds = 10;
+            Random random = new Random();
+
+            for (int i = 0; i < maxRounds; i++)
+            {
+                round++;
+
+                Console.WriteLine($"Round: {round}/{maxRounds}");
+                Console.WriteLine("Press any key to roll the dice.");
+                Console.ReadKey();
+                playerRandomNum = random.Next(1, 7); //the actual range is 1-6
+
+                Console.WriteLine($"You rolled a {playerRandomNum}");
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000); //makes the system wait 1000 miliseconds before it executes the next code in the for loop which gives anticipation.
+                //1000 miliseconds = 1 second
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine($"Enemy AI rolled a {enemyRandomNum}");
+                
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player wins this round!");
+
+                } 
+                else if (playerRandomNum < enemyRandomNum)
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy wins this round!");
+                }
+                else
+                {
+                    Console.WriteLine("Draw!");
+                }
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine($"The score is now - Player: {playerPoints} | Enemy: {enemyPoints} ");
+                Console.WriteLine("======================================================");
+            }
+
+            if (playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerPoints < enemyPoints)
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("Draw!");
+            }
+            Console.ReadKey();
+            
+
+        }
+    }
+}
